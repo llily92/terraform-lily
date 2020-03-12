677ff713a2ec34712f907d95d8eb09e4e2d56ed8
Cloud User <centos@ip-172-31-93-244.ec2.internal>
2020-03-12 22:39:14 +0000

    vpc in different regions

--- terraform-lily/vpc/backend.tf
+++ terraform-lily/vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-lily"
-key = "tower/us-east-1/tools/ohio/tower.tfstate"
+key = "tower/us-east-1/tools/virginia/tower.tfstate"
 region = "us-east-1"
   }
 }
